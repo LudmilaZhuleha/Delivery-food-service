33604d9a0b44db8a268aae150e1904e8c42ab954
LudmilaZhuleha <zhuleha@lir.by>
2021-11-07 20:40:57 +0300

    dynamic rendering added

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,54 @@
-const restaurant = 'food-band';
 
-const renderItems = (data)=>{
-    console.log(data);
+const cardsMenu = document.querySelector('.cards-menu');
+
+const changeTitle =(restaurant)=>{
+    const restaurantTitle = document.querySelector('.restaurant-title');
+    restaurantTitle.textContent = restaurant.name;
 }
 
-fetch(`./db/${restaurant}.json`)
-.then((response)=>response.json())
-.then((data)=>{
-    renderItems(data)
-})
-.catch((error)=>{
-    console.log(error);
-})
+const renderItems = (data)=>{
+   data.forEach(({description, id, price, image, name})=>{
+       const card =document.createElement('div');
+       card.classList.add('card');
+       
+       card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="ingredients">${description}</div>
+                </div>
+                <!-- /.card-info -->
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price}</strong>
+                </div>
+            </div>
+       `
+       cardsMenu.append(card);
+   })
+}
+
+if(localStorage.getItem('restaurant')){
+    const restaurant = JSON.parse((localStorage.getItem('restaurant')));
+
+    changeTitle(restaurant);
+
+    fetch(`./db/${restaurant.products}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+        renderItems(data)
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+} else {
+    window.location.href = '/';
+}
+
